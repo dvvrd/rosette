7d9af9c3623ecc7ee340ffc1a17a6ccd56cb2655
Grigory Fedyukovich <grigory.fedyukovich@gmail.com>
2016-11-28 14:11:42 -0800

    10 successful tests for power/mult (no merge)

---
+++ test/unbound/lra/power.rkt
@@ -0,0 +1,68 @@
+#lang rosette/unbound
+
+(require rackunit rackunit/text-ui rosette/lib/roseunit)
+
+(current-bitwidth #f)
+(define-symbolic x y a b integer?)
+
+(define/unbound (mult x y) (~> integer? integer? integer?)
+  (if (= x 0) 0
+      (+ y (mult (- x 1) y))))
+
+(define/unbound (pwr a x) (~> integer? integer? integer?)
+  (if (= a 0) 1
+      (mult x (pwr (- a 1) x))))
+
+(define pwr-tests
+  (test-suite+
+   "[unbound] Tests for lra/pwr.rkt"
+
+   (check-unsat
+     (verify/unbound (assert (= (mult 23 10) 230)))
+   )
+
+   (check-sat
+     (verify/unbound (assert (= (mult 23 11) 23)))
+   )
+
+   (check-sat
+     (verify/unbound #:assume    (assert (and (> a 0) (> x 0)))
+                     #:guarantee (assert (> (mult a x) a)))
+   )
+
+   (check-unsat
+     (verify/unbound #:assume    (assert (and (> a 1) (> x 1)))
+                     #:guarantee (assert (let ([b (mult a x)]) (and (> b a) (> b x)))))
+   )
+
+   (check-unsat
+     (verify/unbound #:assume    (assert (and (> a 1) (> x 0)))
+                     #:guarantee (assert (let ([b (mult a x)])(or (> b a) (> b x)))))
+   )
+
+   (check-unsat
+     (verify/unbound (assert (= (pwr 3 2) 8)))
+   )
+
+   (check-sat
+     (verify/unbound (assert (= (pwr 2 3) 8)))
+   )
+
+   (check-sat
+     (verify/unbound #:assume    (assert (> x 2))
+                     #:guarantee (assert (> (pwr 2 x) (mult 3 x))))
+   )
+
+   (check-unsat
+     (verify/unbound #:assume    (assert (and (> x 0) (> a 0)))
+                     #:guarantee (assert (> (pwr a x) 0)))
+   )
+
+   (check-unsat
+     (verify/unbound #:assume    (assert (and (> a 2) (> x 1)))
+                     #:guarantee (assert (> (pwr a x) (* 2 x))))
+   )
+
+))
+
+(time (run-tests pwr-tests))
